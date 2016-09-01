1fb0eecdf8921e9ac003084fdaabede2572729a7
lucas.tomasi <lcstomasi@gmail.com>
2016-08-31 23:29:28 -0300

    Add views

--- src/res/config/functions.sql
+++ src/res/config/functions.sql
@@ -102,3 +102,69 @@ create trigger adjustSequenceFields before insert or update on fields for each r
 -- 	language plpgsql;
 -- 
 -- create trigger adjustSequenceFields after delete on fields for each row execute procedure adjustSequenceFields();
+
+
+
+
+
+CREATE OR REPLACE VIEW view_routes AS
+SELECT tr.id AS id,
+       tr.days AS days,
+       tr.sequence AS sequence,
+       u.name AS USER,
+       s.name AS sector,
+       t.name AS TYPE
+FROM types_routes tr
+LEFT JOIN users u ON (tr.ref_user = u.id)
+LEFT JOIN sectors s ON (tr.ref_sector = s.id)
+INNER JOIN types t ON (tr.ref_type = t.id)
+WHERE tr.state = 0;
+
+
+CREATE OR REPLACE VIEW view_types AS
+SELECT t.id AS id,
+       t.name AS name,
+       c.name AS category
+FROM types t
+INNER JOIN categories c ON (t.ref_category = c.id)
+WHERE t.state = 0;
+
+
+CREATE OR REPLACE VIEW view_fields AS
+SELECT f.id AS id,
+       f.label AS label,
+       CASE
+           WHEN f.TYPE = 0 THEN 'Texto'
+           WHEN f.TYPE = 1 THEN 'Lista'
+           WHEN f.TYPE = 2 THEN 'Número'
+           WHEN f.TYPE = 3 THEN 'Data'
+           WHEN f.TYPE = 4 THEN 'Arquivo'
+           ELSE 'Texto Simples'
+       END AS TYPE,
+       t.name AS type_request,
+       CASE
+           WHEN f.required = 1 THEN 'Sim'
+           ELSE 'Não'
+       END AS required,
+       f.sequence AS sequence
+FROM types t ,
+     fields f
+WHERE t.id = f.ref_type
+  AND f.state = 0;
+
+
+CREATE OR REPLACE VIEW view_requests AS
+SELECT r.id AS id,
+       t.name AS TYPE,
+       u.name AS USER,
+       to_char(dt_end::date ,'DD/MM/YYYY') AS dt_end ,
+       to_char(dt_start::date, 'DD/MM/YYYY') AS dt_start ,
+       CASE
+           WHEN r.state = 1 THEN 'Aprovada'
+           WHEN r.state = 2 THEN 'Cancelada'
+           WHEN r.state = 3 THEN 'Reprovada'
+           ELSE 'Em Andamento'
+       END AS STATE
+FROM requests r
+INNER JOIN types t ON (t.id = r.ref_type)
+INNER JOIN users u ON (u.id = r.ref_user);
